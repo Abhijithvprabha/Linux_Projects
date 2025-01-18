4950f2c720f2927231849cbb36ad073da395bb89
Abhijith Vilangapparayil Prabha <Abhijithvprabha@gmail.com>
2025-01-17 21:18:08 -0800

    service_running

---
+++ Projects/Monitoring_Project/disk_usage.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# Slack webhook URL
+
+SLACK_WEBHOOK_URL=''
+
+# the threshold for disk usage (90%)
+
+THRESHOLD=90
+
+# Get the current disk usage %
+
+DISK_USAGE= $( df -h / | awk 'NR==2 {print $5}' | cut -d% -f1)
+
+# GET THE HSOTNAME 
+
+SERVER_HOSTNAME=$(hostname)
+
+if [ "$DISK_USAGE' -ge '$THRESHOLD"]; then
+MESSAGE="exclamation: ALERT - Disk usage on $SERVER_HOSTNAME is at $DISK_USAGE%."
+
+# Sending the alert message to Slack
+
+curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" "$SLACK_WEBHOOK_URL"
+fi
+
+
